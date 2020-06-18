7a04252a1c7d03a4a5b8f4a52aabcab39fd05c03
Iago Bontempo <iagobontempo@hotmail.com>
2020-06-18 20:51:34 -0300

    att

---
+++ src/server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+const { exec, execFile } = require('child_process'); //Para executar comandos do ubuntu
+const cwebp = require('cwebp-bin');
+
+const app = express();
+
+app.use(express.static('uploads'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, 'uploads/');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/index.html'));
+});
+
+app.post('/profile', upload.single('avatar'), (req, res) => {
+
+    console.log(req.file);
+
+    try {
+        res.send('UPLOAD OK').status(200);
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.get('/delete/:name', (req, res) => {
+    try {
+        let arquivo = req.params.name;
+        fs.unlinkSync((__dirname, `./uploads/${arquivo}`))
+        res.send('DELETE SUCESS!').status(200);
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.get('/compress/:name', (req, res) => {
+
+    let arquivo = req.params.name;
+
+    execFile(cwebp,
+        [`${(__dirname, './uploads/' + arquivo)}`, '-o', `${(__dirname, './uploads/compressed/' + arquivo + '.webp')}`], err => {
+            if (err) {
+                throw err;
+            }
+
+            console.log('Image is converted!')
+            res.send('COMPRESSED').status(200);
+        })
+});
+
+
+app.get('/test', (req, res) => {
+    exec('ls -la', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.send('LS -LA NO CONSOLE OK').status(200);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`)
+    });
+})
+
+app.get('/view', (req, res) => {
+    try {
+        let files = fs.readdirSync((__dirname, './uploads'));
+        files.map((value) => {
+            console.log(value);
+        })
+        console.log(files);
+        res.send('CONSOLE')
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+const port = 3333;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
